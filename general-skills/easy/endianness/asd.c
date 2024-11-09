ae4797d9491b9cc0bbf626871038e6afe4cc20f3
Gioele Bernardini <gioele.g@outlook.com>
2024-11-09 22:26:27 +0100

    Update: begin writing exploit for the challenge

---
+++ general-skills/easy/endianness/asd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <word>\n", argv[0]);
+    exit(1);
+  }
+
+  size_t len = strlen(argv[1]);
+
+  // Big Endian
+  for (size_t i = 0; i < len; i++) {
+    printf("\\x%02x", (unsigned char)argv[1][i]);
+  }
+
+  printf("\n\n");
+
+  // Little Endian
+  for (int i = (int)len - 1; i >= 0; i--) {
+    printf("\\x%02x", (unsigned char)argv[1][i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
+
